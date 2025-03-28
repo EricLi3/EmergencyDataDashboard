6ff8d0d28b29c5f2d85b4211e42118ec15680941
Eric Li <ericlibeibei@gmail.com>
2025-03-27 21:06:08 -0400

    Added a "flow chart" type thing

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -7,11 +7,14 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          📊 Goyco Data Dashboard
+          Goyco Data Dashboard
         </Link>
         <ul className="nav-menu">
           <li className="nav-item">
             <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/guides" className="nav-link">Guides</Link>
           </li>
           <li className="nav-item">
             <Link to="/whatsapp" className="nav-link">WhatsApp Numbers</Link>
--- Frontend/src/components/Workflow/Workflow.jsx
+++ Frontend/src/components/Workflow/Workflow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Deliverables from "../Deliverables/Deliverables";
 
 const Workflow = () => {
-    const deliverablesData = [
+    const deliverablesDataForms = [
         {
             image: "/images/Onboarding.png",
             title: "Enrollment Form",
@@ -11,7 +11,6 @@ const Workflow = () => {
             userLink: "https://forms.gle/VU8Kfc4ebhaBVkM88",
             editorLink: "https://docs.google.com/forms/d/1St-ysQXoN3uqIPpJBIMB_OEir59ghMUls2d52-kSaac/edit"
         },
-
         {
             image: "/images/WellnessCheck.png",
             title: "Wellness Check",
@@ -20,6 +19,9 @@ const Workflow = () => {
             userLink: "https://forms.gle/qJUmPJ2e5qDvvPQb7",
             editorLink: "https://docs.google.com/forms/d/1_qHY8mxjrpwWcIgOvlc3I1kyVcVxAX22Adls88qrk1w/edit"
         },
+    ];
+
+    const deliverablesDataSheets = [
         {
             image: "/images/MasterSheets.png",
             title: "Master Sheet",
@@ -27,7 +29,10 @@ const Workflow = () => {
             text: "This Google Sheets contains all the data collected from the forms",
             userLink: "",
             editorLink: "https://docs.google.com/spreadsheets/d/1dTDd8Xwg3wv7MRHG2WAX_vXZKlEFne_rDMeemcHSD3s/edit?gid=1949800455#gid=1949800455"
-        },
+        }
+    ];
+
+    const deliverablesDataMaps = [
         {
             image: "/images/MasterMap2024.png",
             title: "Master Map",
@@ -35,24 +40,63 @@ const Workflow = () => {
             text: "This Google MyMaps contains useful demographic information on residents.",
             userLink: "",
             editorLink: "https://docs.google.com/spreadsheets/d/1dTDd8Xwg3wv7MRHG2WAX_vXZKlEFne_rDMeemcHSD3s/edit?gid=1949800455#gid=1949800455"
-        },
+        }
     ];
 
     return (
-        <div>
+        <div style={{ textAlign: "center", margin: "20px" }}>
             <h1>Forms → Sheets → Maps</h1>
-            <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-                {deliverablesData.map((data, index) => (
-                    <Deliverables
-                        key={index}
-                        image={data.image}
-                        title={data.title}
-                        subtitle={data.subtitle}
-                        text={data.text}
-                        userLink={data.userLink}
-                        editorLink={data.editorLink}
-                    />
-                ))}
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "20px" }}>
+                {/* Forms Section */}
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    {deliverablesDataForms.map((data, index) => (
+                        <Deliverables
+                            key={index}
+                            image={data.image}
+                            title={data.title}
+                            subtitle={data.subtitle}
+                            text={data.text}
+                            userLink={data.userLink}
+                            editorLink={data.editorLink}
+                        />
+                    ))}
+                </div>
+                {/* Arrow pointing to Sheets */}
+                <div style={{ margin: "0 20px" }}>
+                    <span style={{ fontSize: "2rem", color: "#6B7280" }}>→</span>
+                </div>
+                {/* Sheets Section */}
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    {deliverablesDataSheets.map((data, index) => (
+                        <Deliverables
+                            key={index}
+                            image={data.image}
+                            title={data.title}
+                            subtitle={data.subtitle}
+                            text={data.text}
+                            userLink={data.userLink}
+                            editorLink={data.editorLink}
+                        />
+                    ))}
+                </div>
+                {/* Arrow pointing to Maps */}
+                <div style={{ margin: "0 20px" }}>
+                    <span style={{ fontSize: "2rem", color: "#6B7280" }}>→</span>
+                </div>
+                {/* Maps Section */}
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    {deliverablesDataMaps.map((data, index) => (
+                        <Deliverables
+                            key={index}
+                            image={data.image}
+                            title={data.title}
+                            subtitle={data.subtitle}
+                            text={data.text}
+                            userLink={data.userLink}
+                            editorLink={data.editorLink}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
