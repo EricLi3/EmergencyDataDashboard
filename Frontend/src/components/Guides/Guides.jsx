4ea1ce8eaa713d3c9692c89f6ab6548027180386
Eric Li <ericlibeibei@gmail.com>
2025-03-28 17:35:24 -0400

    guides for sheets forms and maps

--- Frontend/src/components/Guides/Guides.jsx
+++ Frontend/src/components/Guides/Guides.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const Guides = () => {
-    const videos = [
+    const links = [
         {
             title: 'Google Sheets Tutorial',
-            url: 'https://www.youtube.com/embed/exampleSheetsVideoID',
+            url: 'https://support.google.com/a/users/answer/9282959',
         },
         {
             title: 'Google Forms Tutorial',
-            url: 'https://www.youtube.com/embed/exampleFormsVideoID',
+            url: 'https://support.google.com/docs/answer/6281888?hl=en&co=GENIE.Platform%3DDesktop',
         },
         {
             title: 'Google Maps Tutorial',
-            url: 'https://www.youtube.com/embed/exampleMapsVideoID',
+            url: 'https://bpb-us-w2.wpmucdn.com/wp.wpi.edu/dist/e/203/files/2023/05/WPI-Fondita-My-Maps-Facilitators-Guide.pdf',
         },
     ];
 
@@ -20,18 +20,12 @@ const Guides = () => {
         <div>
             <h1>Guides</h1>
             <div>
-                {videos.map((video, index) => (
+                {links.map((link, index) => (
                     <div key={index} style={{ marginBottom: '20px' }}>
-                        <h2>{video.title}</h2>
-                        <iframe
-                            width="560"
-                            height="315"
-                            src={video.url}
-                            title={video.title}
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
+                        <h2>{link.title}</h2>
+                        <a href={link.url} target="_blank" rel="noopener noreferrer">
+                            Open Guide
+                        </a>
                     </div>
                 ))}
             </div>
