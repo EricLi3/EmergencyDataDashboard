163b875bd8c48563a120813b2e14eaaadfc17043
Eric Li <ericlibeibei@gmail.com>
2025-03-29 20:33:47 -0400

    fix backend axios config

--- Frontend/src/axios/axiosConfig.js
+++ Frontend/src/axios/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://emergencydatadashboard.onrender.com/',
-  baseURL: 'http://127.0.0.1:5000/',
+  baseURL: 'https://emergencydatadashboard.onrender.com/',
+  // baseURL: 'http://127.0.0.1:5000/',
 });
 
 export default instance;
