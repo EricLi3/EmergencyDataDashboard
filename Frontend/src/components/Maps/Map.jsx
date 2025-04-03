70eb1086b1ab919daeae977bd25c5fa360f86d40
Eric Li <ericlibeibei@gmail.com>
2025-04-03 10:47:10 -0400

    Add flood risk map from FEMA

--- Frontend/src/components/Maps/Map.jsx
+++ Frontend/src/components/Maps/Map.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
 
 const Map = () => {
     return (
         <div>
             <h1>Map</h1>
-            <iframe 
-                src="https://www.google.com/maps/d/u/0/embed?mid=1kDED7vqz9AogrU1Gtooek6W2IZ8koY8&ehbc=2E312F&noprof=1" 
-                width="940" 
+            <iframe
+                src="https://www.google.com/maps/d/u/0/embed?mid=1kDED7vqz9AogrU1Gtooek6W2IZ8koY8&ehbc=2E312F&noprof=1"
+                width="940"
                 height="680"
                 style={{ border: 0 }}
                 allowFullScreen=""
                 loading="lazy"
                 title="Master Maps"
             ></iframe>
+
+            <Grid container justifyContent="center" sx={{ mt: 4 }}>
+                <img
+                    src="/images/flood_risk.png"
+                    alt="Flood Risk Map"
+                    style={{ width: '100%', maxWidth: '1200px', borderRadius: '8px' }}
+                />
+            </Grid>
         </div>
     );
 };
