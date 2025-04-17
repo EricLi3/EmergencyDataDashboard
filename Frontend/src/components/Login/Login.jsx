22575dd53d0684adb061ae01d1c0f7f34eb7b887
Eric Li <ericlibeibei@gmail.com>
2025-04-17 16:17:11 -0400

    Use deployed backend

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -9,7 +9,7 @@ const Login = ({ onLogin }) => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://127.0.0.1:5000/login", {
+            const response = await axios.post("https://emergencydatadashboard.onrender.com/login", {
                 username,
                 password,
             });
